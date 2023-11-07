a4942232fb22679f42011d4a037c352dca191b52
jaycossey <jaycossey@live.com>
2023-11-07 13:38:44 +0000

    fixed text display in bio, fixed card position

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,7 +1,8 @@
-import meetStereoText from './textContent';
+// GLOBALS ---------------------------------------------------------
+const meetStereoEl = document.querySelector('#meetStereo');
 
-const meetStereoEl = document.getElementById('meetStereo');
+const meetStereoText = "Northamtonshire's odd one out that comes around once.\n\nThe wide-ranging repetoire of this four-piece includes Progressive, Industrial, Melodic Death Metal and Orchestral music, still, with a twist - to Skulls, every song is an experiment, an exercise in nonconformity from the creative minds of Aivars and the balanced, locical mind of Arthur.\n\nThe band is well known for an infamous Bat Incident of their ex-drummer that went viral. They're branded and the 'local heroes', 'our own Randy Blythe', and 'overweight Dani Filth'. Did we mention killer live shows?\n\nSo, if you are a fan of bands such as Fear Factory, Mudvayne and Static-X, enter your name and email-address below and you will recieve a download to our latest three singles.";
 
-meetStereoEl.innerText = meetStereoText;
+meetStereoEl.textContent = meetStereoText;
 
 console.log(meetStereoEl);
